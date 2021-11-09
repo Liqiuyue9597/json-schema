29f93fb252de0e659e5a413c27b57ce2fb027fdf
elissali <elissali@tencent.com>
2021-11-09 22:03:23 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
-import { createApp } from "vue";
+import { createApp, defineComponent, createVNode } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const img = require("./assets/logo.png");
 import App from "./App.vue";
+
+// const App = defineComponent({
+//   render() {
+//     return createVNode("div", { id: "app" }, [
+//       createVNode("img", {
+//         alt: "Vue logo",
+//         src: img,
+//       }),
+//       createVNode(HelloWorld, {
+//         msg: "Welcome to Your Vue.js + TypeScript App",
+//       }),
+//     ]);
+//   },
+// });
 
 createApp(App).mount("#app");
